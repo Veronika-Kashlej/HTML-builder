2d79b5c396d680072f08ac22a66c52b27c18c742
Veronika Kashlej <veronika.kashlei@mail.ru>
2025-01-21 11:55:42 +0300

    fix: 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,6 +4,10 @@ const path = require("path");
 const output = fs.createWriteStream(path.join(__dirname, "text.txt"));
 stdout.write("Enter text:\n");
 stdin.on("data", (data) => {
+    if(data.toString().trim() === 'exit'){
+        stdout.write("\nGoodbye!");
+        process.exit();
+    }
     output.write(data);
 });
 process.on("SIGINT", () => {
